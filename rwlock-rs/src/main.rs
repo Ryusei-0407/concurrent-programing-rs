070d90363eeb85261abe373224b4695eca32a622
Ryusei-0407 <ryu000407@gmail.com>
2022-01-20 01:46:00 +0900

    add comment

--- rwlock-rs/src/main.rs
+++ rwlock-rs/src/main.rs
@@ -3,28 +3,32 @@ use std::sync::RwLock;
 fn main() {
     let lock = RwLock::new(10);
     {
+        // lock
         let mut v = lock.write().unwrap();
         *v = 7;
         println!("v = {v}");
-    }
+    } // unlock
 
     {
+        // lock
         let v1 = lock.read().unwrap();
         let v2 = lock.read().unwrap();
         println!("v1 = {v1}");
         println!("v2 = {v2}");
-    }
+    } // unlock
 
     {
+        // lock
         let mut v = lock.write().unwrap();
         *v = 123;
         println!("v = {v}");
-    }
+    } // unlock
 
     {
+        // lock
         let v1 = lock.read().unwrap();
         let v2 = lock.read().unwrap();
         println!("v1 = {v1}");
         println!("v2 = {v2}");
-    }
+    } // unlock
 }
