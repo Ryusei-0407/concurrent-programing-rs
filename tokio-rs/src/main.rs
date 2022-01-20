b2360926e09a7d5fd97f6575c006cf95e829eb40
ryu <ryu000407@gmail.com>
2022-01-21 07:04:33 +0900

    sleep

--- tokio-rs/src/main.rs
+++ tokio-rs/src/main.rs
@@ -1,43 +1,9 @@
-use tokio::io::{AsyncBufReadExt, AsyncWriteExt};
-use tokio::io;
-use tokio::net::TcpListener;
+use std::{thread, time};
 
 #[tokio::main]
-async fn main() -> io::Result<()> {
-    let host = TcpListener::bind("127.0.0.1:10000").await.unwrap();
-
-    loop {
-        // Accept connection
-        let (mut socket, addr) = host.accept().await?;
-        println!("accept: {addr}");
-
-        // Spawn
-        tokio::spawn(async move {
-            // Create buffer read/write object
-            let (r, w) = socket.split(); // Split Read socket / Write socket
-            let mut reader = io::BufReader::new(r);
-            let mut writer = io::BufWriter::new(w);
-
-            let mut line = String::new();
-            loop {
-                line.clear(); // Clear
-                // async read line
-                match reader.read_line(&mut line).await {
-                    Ok(0) => {
-                        println!("closed: {addr}");
-                        return;
-                    }
-                    Ok(_) => {
-                        print!("read: {addr}, {line}");
-                        writer.write_all(line.as_bytes()).await.unwrap();
-                        writer.flush().await.unwrap();
-                    }
-                    Err(e) => {
-                        println!("error: {addr}, {e}");
-                        return;
-                    }
-                }
-            }
-        });
-    }
+async fn main() {
+    tokio::join!(async move {
+        let ten_secs = time::Duration::from_secs(10);
+        thread::sleep(ten_secs);
+    });
 }
