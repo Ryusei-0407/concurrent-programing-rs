55a71e3b028e9d7fdb8732add4e6934535088e32
ryu <ryu000407@gmail.com>
2022-01-21 07:10:10 +0900

    tokio sleep

--- tokio-rs/src/main.rs
+++ tokio-rs/src/main.rs
@@ -1,9 +1,9 @@
-use std::{thread, time};
+use std::time;
 
 #[tokio::main]
 async fn main() {
     tokio::join!(async move {
         let ten_secs = time::Duration::from_secs(10);
-        thread::sleep(ten_secs);
+        tokio::time::sleep(ten_secs).await;
     });
 }
